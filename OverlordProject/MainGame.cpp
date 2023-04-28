d558b2b40ff2198236443d423c64bd0467b63f25
Alynxx <amberperard@hotmail.com>
2023-04-28 10:06:33 +0200

    Finished week 8 - Shadows

--- OverlordProject/MainGame.cpp
+++ OverlordProject/MainGame.cpp
@@ -7,12 +7,12 @@
 // #define W5
 // #define W6
 // #define W7
-// #define W8
+ #define W8
 // #define W9
 // #define W10
 
 /*MILESTONE Content*/
-#define MILESTONE_1
+// #define MILESTONE_1
 // #define MILESTONE_2
 
 #pragma region Lab/Milestone Includes
@@ -85,9 +85,9 @@ void MainGame::OnGamePreparing(GameContext& gameContext)
 	//gameContext.windowWidth=... (default is 1280)
 	//gameContext.windowHeight=... (default is 720)
 
-	gameContext.windowTitle = L"GP2 - Milestone 1 (2023) | (2DAE08) Perard Amber";
-	//gameContext.windowTitle = L"GP2 - Milestone 2 (2023) | (2DAE13) Doe John";
-	//gameContext.windowTitle = L"GP2 - Exam Project (2023) | (2DAE13) Doe John";
+	//gameContext.windowTitle = L"GP2 - Milestone 1 (2023) | (2DAE08) Perard Amber";
+	gameContext.windowTitle = L"GP2 - Milestone 2 (2023) | (2DAE08) Perard Amber";
+	//gameContext.windowTitle = L"GP2 - Exam Project (2023) | (2DAE08) Perard Amber";
 }
 
 void MainGame::Initialize()
